2329ec8abae8095737c5718be7823e25c7320357
mikasa-girly <fitia.solofonirina@gmail.com>
2026-06-13 17:19:03 +0300

    Update employee.sql

--- employee.sql
+++ employee.sql
@@ -1,3 +1,8 @@
-/opt/lampp/bin/mysql -u root -p  employees
+installer BDD :
+    sudo /opt/lampp/bin/mysql -u root -p  employees.sql
+tester BDD :
+    sudo /opt/lampp/bin/mysql -t  -u root -p  test_employees_md5.sql
+entrer das BDD :
+    /opt/lampp/bin/mysql -u root -p  employees
 
 SELECT * from departments ;
